c307c4bd42da29e03ad78dfc34574a8b704a89a9
unw10181 <uchennanwilliams@gmail.com>
2026-02-18 03:51:08 -0500

    adding images

--- uchenna-portfolio/src/App.tsx
+++ uchenna-portfolio/src/App.tsx
@@ -1,9 +1,35 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 100,
+      once: false,
+      offset: 100,
+    });
+    document.documentElement.classList.add("dark");
+  }, []);
+
+  const toggleDarkMode = () => {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    document.documentElement.classList.toggle("dark");
+  };
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Dan Portfolio</h1>
+    <div
+      className={
+        darkMode
+          ? "bg-linear-to-br from-gray-900 via-[#0d182e] to-gray-900"
+          : "bg-linear-to-br from-gray-50 to-blue-50 min-h-screen "
+      }
+    >
+      <Navbar />
     </div>
   );
 }
